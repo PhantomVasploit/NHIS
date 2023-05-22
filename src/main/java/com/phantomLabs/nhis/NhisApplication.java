23b5301b88a2e250413baaf910da45bcc2c76346
PhantomVasploit <paulvasgit99@gmail.com>
2023-05-22 15:34:43 +0300

    Project initialization

---
+++ src/main/java/com/phantomLabs/nhis/NhisApplication.java
@@ -0,0 +1,13 @@
+package com.phantomLabs.nhis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phantomLabs/nhis/NhisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantomLabs.nhis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
